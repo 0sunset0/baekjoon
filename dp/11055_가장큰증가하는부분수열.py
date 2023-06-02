5509d217b7da98c00423461cdd93a99f3acb41dd
0sunset0 <chi6465618@naver.com>
2023-06-03 03:42:39 +0900

    dp

---
+++ dp/11055_가장큰증가하는부분수열.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int, input().split()))
+dp = [0 for i in range(n)]
+dp[0] = a[0]
+
+for i in range(n):
+    for j in range(i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], a[i] + dp[j])
+        else:
+            dp[i] = max(dp[i], a[i])
+print(dp)
